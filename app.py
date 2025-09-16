78d9006fef410991ce244096fbca43d29d1f1ec6
Shridhar <shridharcbe@gmail.com>
2025-09-16 11:54:45 +0530

    added render free port

--- app.py
+++ app.py
@@ -175,4 +175,5 @@ def director_collaborators(person_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render gives PORT, fallback to 5000 locally
+    app.run(host="0.0.0.0", port=port, debug=True)
